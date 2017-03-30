fe3cadaff31070aef7b8334c07352ccebc2fdd49
lshannon <luke.shannon@gmail.com>
2017-03-30 15:10:52 -0400

    Cleaned up scripts

--- pws-scdf-setup.sh
+++ pws-scdf-setup.sh
@@ -1,27 +1,43 @@
-echo " $$$$$$\   $$$$$$\  $$$$$$$\  $$$$$$$$\       $$$$$$$\                                    ";
-echo "$$  __$$\ $$  __$$\ $$  __$$\ $$  _____|      $$  __$$\                                   ";
-echo "$$ /  \__|$$ /  \__|$$ |  $$ |$$ |            $$ |  $$ | $$$$$$\  $$$$$$\$$$$\   $$$$$$\  ";
-echo "\$$$$$$\  $$ |      $$ |  $$ |$$$$$\          $$ |  $$ |$$  __$$\ $$  _$$  _$$\ $$  __$$\ ";
-echo " \____$$\ $$ |      $$ |  $$ |$$  __|         $$ |  $$ |$$$$$$$$ |$$ / $$ / $$ |$$ /  $$ |";
-echo "$$\   $$ |$$ |  $$\ $$ |  $$ |$$ |            $$ |  $$ |$$   ____|$$ | $$ | $$ |$$ |  $$ |";
-echo "\$$$$$$  |\$$$$$$  |$$$$$$$  |$$ |            $$$$$$$  |\$$$$$$$\ $$ | $$ | $$ |\$$$$$$  |";
-echo " \______/  \______/ \_______/ \__|            \_______/  \_______|\__| \__| \__| \______/ ";
-echo "                                                                                          ";
-echo "                                                                                          ";
-echo "                                                                                          ";
+echo "*****************************"
+echo "* 			SCDF Demo						*"
+echo "*****************************"
 
 APP_NAME=luke-dataflow-server
 
+# Read In Sensitive Data
+
+echo "For the Admin Server to be able to create micro services for the stream we need the following:"
+
+echo -n 'Enter the PWS Username:'
+read -s USERNAME
+echo
+
+echo -n 'Enter the PWS Password:'
+read -s PASSWORD
+echo
+
+echo -n 'Enter the PWS Organization:'
+read -s ORG
+echo
+
+echo -n 'Enter the PWS Space:'
+read -s SPACE
+echo
+
 echo "** Make sure you are logged into to PWS in a Space that will accomidate this deployment"
+echo
 
 echo "Creating the required Redis Service"
 cf create-service rediscloud 30mb redis
+echo
 
 echo "Creating the required Rabbit Service"
 cf create-service cloudamqp lemur rabbit
+echo
 
 echo "Creating the required MySql Service"
 cf create-service cleardb spark mysql
+echo
 
 if [ ! -f spring-cloud-dataflow-server-cloudfoundry-1.1.1.RELEASE.jar ]; then
 	echo "Downloading the Server App for Pivotal Cloud Foundry. This will be deployed in Cloud Foundry"
@@ -32,18 +48,23 @@ if [ ! -f spring-cloud-dataflow-shell-1.1.1.RELEASE.jar ]; then
 	echo "Downloading the Shell Application to run locally to connect to the server in PCF"
 	wget http://repo.spring.io/release/org/springframework/cloud/spring-cloud-dataflow-shell/1.1.1.RELEASE/spring-cloud-dataflow-shell-1.1.1.RELEASE.jar
 fi
+echo
 
 echo "Pusing the Server to PCF"
 cf push $APP_NAME --no-start -p spring-cloud-dataflow-server-cloudfoundry-1.1.1.RELEASE.jar
+echo
 
 echo "Binding the Redis Service to the Server"
 cf bind-service $APP_NAME redis
+echo
 
 echo "Binding the Rabbit Service to the Server"
 cf bind-service $APP_NAME rabbit
+echo
 
 echo "Binding the MySql  Service to the Server"
 cf bind-service $APP_NAME mysql
+echo
 
 echo "Setting the environmental variables"
 cf set-env $APP_NAME MAVEN_REMOTE_REPOSITORIES_REPO1_URL https://repo.spring.io/libs-snapshot
@@ -52,12 +73,14 @@ cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_DOMAIN cfapps.io
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_STREAM_SERVICES rabbit
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SKIP_SSL_VALIDATION false
 cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SERVICES redis,rabbit
+echo "Setting Env for Username and Password silently"
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_USERNAME $USERNAME > /dev/null
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_PASSWORD $PASSWORD > /dev/null
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_ORG $ORG
+cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SPACE $SPACE
 
-echo "Run: cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_USERNAME *********
-echo "Run: cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_PASSWORD *********"
-echo "Run: cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_ORG '*********' "
-echo "Run: cf set-env $APP_NAME SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_SPACE *******"
-
-echo "Run cf start $APP_NAME"
+echo "Starting Up App"
+cf start $APP_NAME
+echo
 
 echo "Set Up Complete"
